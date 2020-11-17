a13c6c04a14a0e64cde1d9e35757e0d0b88b160a
Yangyang Fu <yangyang.fu@tamu.edu>
2020-11-17 12:52:36 -0600

    Add scripts for compiling Modelica into fmu in linux

--- testcases/1zone/compile_fmu.py
+++ testcases/1zone/compile_fmu.py
@@ -1,40 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-This module compiles the defined test case model into an FMU using the
-overwrite block parser.
+This module compiles Modelica models into fmu models.
 
-The following libraries must be on the MODELICAPATH:
-
-- Modelica IBPSA
-- Modelica Buildings
+@ Yangyang Fu, yangyang.fu@tamu.edu
 
 """
 
-from parsing import parser
+
+from pymodelica import compile_fmu
+# DEFINE MODEL
+# ------------
+mopath = 'SingleZoneVAV.mo'
+modelpath = 'SingleZoneVAV.TestCaseSupervisory'
+# ------------
+
+# COMPILE FMU: set JVM maximum leap to 1G to avoid memory issues
+# -----------
+# the defauted compilation target is model exchange, where no numerical integrator is integrated into the fmu. 
+# The equations in FMU is solved by numerical solvers in the importing tool.
+
+fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneVAV.fmu')
+# -----------
 
 
-def compile_fmu():
-    '''Compile the fmu.
-    
-    Returns
-    -------
-    fmupath : str
-        Path to compiled fmu.
-    
-    '''
-    
-    # DEFINE MODEL
-    # ------------
-    mopath = 'SingleZoneVAV.mo';
-    modelpath = 'SingleZoneVAV.TestCaseSupervisory'
-    # ------------
-    
-    # COMPILE FMU
-    # -----------
-    fmupath = parser.export_fmu(modelpath, [mopath])
-    # -----------
-    
-    return fmupath
 
-if __name__ == "__main__":
-    fmupath = compile_fmu()
---
+++ testcases/1zone/compile_fmu.sh
@@ -0,0 +1,12 @@
+  exec docker run \
+ 	  --name fmuc \
+          --user=root \
+	  --detach=false \
+	  -e DISPLAY=${DISPLAY} \
+	  -v /tmp/.X11-unix:/tmp/.X11-unix\
+	  --rm \
+	  -v `pwd`:/mnt/shared \
+	  -i \
+          -t \
+	  mpcdrl /bin/bash -c "cd /mnt/shared && python /mnt/shared/compile_fmu.py"
+    exit $
