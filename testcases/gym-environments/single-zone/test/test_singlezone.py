b35fea7322297bd67f27cf5fe8aa80747c8b2172
Yangyang Fu <yangyang.fu@tamu.edu>
2020-12-07 15:11:39 -0600

    Improved folder structure

--- testcases/gym-environments/single-zone/test/test_singlezone.py
+++ testcases/gym-environments/single-zone/test/test_singlezone.py
@@ -11,7 +11,7 @@ import numpy as np
 import math
 import time
 
-def cart_pole_train_qlearning(singlezone_env, max_number_of_steps=500, n_episodes=4, visualize=False):
+def train_qlearning(singlezone_env, max_number_of_steps=500, n_episodes=4, visualize=False):
     """
     Runs one experiment of Q-learning training on cart pole environment
     :param singlezone_env: environment RL agent will learn on.
@@ -27,12 +27,15 @@ def cart_pole_train_qlearning(singlezone_env, max_number_of_steps=500, n_episode
     episode_lengths = np.array([])
 
     #bins of observation space - state space
-    x_bins = _get_bins(-2.4, 2.4, 10)
-    x_dot_bins = _get_bins(-1, 1, 10)
-    phi_bins = _get_bins(78/180*math.pi, 102/180*math.pi, 10)
-    phi_dot_bins = _get_bins(-2, 2, 10)
+    ts = singlezone_env.simulation_start_time
+    te = ts + max_number_of_steps*singlezone_env.tau
 
-    learner = QLearner(n_states=10 ** n_outputs,
+    TZon_bins = _get_bins(273.15+10, 273.15+26, 4)
+    TOut_bins = _get_bins(273.15+0, 273.15+40, 4)
+    solar_bins = _get_bins(0, 2000, 4)
+    power_bins = _get_bins(0, 10000, 4)
+    
+    learner = QLearner(n_states=4**n_outputs,
                        n_actions=singlezone_env.action_space.n,
                        learning_rate=0.2,
                        discount_factor=1,
@@ -40,12 +43,31 @@ def cart_pole_train_qlearning(singlezone_env, max_number_of_steps=500, n_episode
                        exploration_decay_rate=0.99)
 
     for episode in range(n_episodes):
-        x, x_dot, phi, phi_dot = singlezone_env.reset()
-
-        state = _get_state_index([_to_bin(x, x_bins),
-                                  _to_bin(phi, phi_bins),
-                                  _to_bin(x_dot, x_dot_bins),
-                                  _to_bin(phi_dot, phi_dot_bins)])
+        observes = singlezone_env.reset()
+        print(observes)
+        TZon, TOut, solRad, powTot, TOut_pred1, TOut_pred2, TOut_pred3, solRad_pred1, solRad_pred2, solRad_pred3 = observes
+        state = _get_state_index([_to_bin(TZon, TZon_bins),
+                                    _to_bin(TOut, TOut_bins),
+                                    _to_bin(solRad, solar_bins),
+                                    _to_bin(powTot, power_bins),
+                                    _to_bin(TOut_pred1, TOut_bins),
+                                    _to_bin(TOut_pred2, TOut_bins),
+                                    _to_bin(TOut_pred3, TOut_bins),
+                                    _to_bin(solRad_pred1, solar_bins),
+                                    _to_bin(solRad_pred2, solar_bins),   
+                                    _to_bin(solRad_pred3, solar_bins)])
+        print([_to_bin(TZon, TZon_bins),
+                                    _to_bin(TOut, TOut_bins),
+                                    _to_bin(solRad, solar_bins),
+                                    _to_bin(powTot, power_bins),
+                                    _to_bin(TOut_pred1, TOut_bins),
+                                    _to_bin(TOut_pred2, TOut_bins),
+                                    _to_bin(TOut_pred3, TOut_bins),
+                                    _to_bin(solRad_pred1, solar_bins),
+                                    _to_bin(solRad_pred2, solar_bins),   
+                                    _to_bin(solRad_pred3, solar_bins)])
+    
+        print(state)
 
         action = learner.set_initial_state(state)
 
@@ -55,13 +77,20 @@ def cart_pole_train_qlearning(singlezone_env, max_number_of_steps=500, n_episode
 
             observation, reward, done, _ = singlezone_env.step(action)
 
-            x, x_dot, phi, phi_dot = observation
-            state_prime = _get_state_index([_to_bin(x, x_bins),
-                                            _to_bin(phi, phi_bins),
-                                            _to_bin(x_dot, x_dot_bins),
-                                            _to_bin(phi_dot, phi_dot_bins)])
+            TZon, TOut, solRad, powTot, TOut_pred1, TOut_pred2, TOut_pred3, solRad_pred1, solRad_pred2, solRad_pred3 = observation
+            state_prime = _get_state_index([_to_bin(TZon, TZon_bins),
+                                    _to_bin(TOut, TOut_bins),
+                                    _to_bin(solRad, solar_bins),
+                                    _to_bin(powTot, power_bins),
+                                    _to_bin(TOut_pred1, TOut_bins),
+                                    _to_bin(TOut_pred2, TOut_bins),
+                                    _to_bin(TOut_pred3, TOut_bins),
+                                    _to_bin(solRad_pred1, solar_bins),
+                                    _to_bin(solRad_pred2, solar_bins),   
+                                    _to_bin(solRad_pred3, solar_bins)])
 
-            action = learner.learn_observation(state_prime, reward)
+            rew = reward[0][0]+reward[1][0]
+            action = learner.learn_observation(state_prime, rew)
 
             if done or step == max_number_of_steps - 1:
                 episode_lengths = np.append(episode_lengths, int(step + 1))
@@ -109,87 +138,45 @@ def _get_state_index(state_bins):
     Each integer is an index of bin, where corresponding variable belongs.
     :return: integer value corresponding to the environment state
     """
-    state = int("".join(map(lambda state_bin: str(state_bin), state_bins)))
-    return state
 
+    #state = int("".join(map(lambda state_bin: str(state_bin), state_bins)))
+    state = 0
+
+    for i, state_bin in zip(range(len(state_bins)),state_bins):
+        state += state_bin*4**i
+    return state
 
 def run_ql_experiments(n_experiments=1,
                        n_episodes=10,
                        visualize=False,
-                       m_cart=10,
-                       m_pole=1,
-                       theta_0=85/180*math.pi,
-                       theta_dot_0=0,
-                       time_step=0.05,
-                       positive_reward=1,
-                       negative_reward=-100,
-                       force=12,
-                       log_level=logging.DEBUG):
-    """
-    Wrapper for running experiment of q-learning training on cart pole environment.
-    Is responsible for environment creation and closing, sets all necessary parameters of environment.
-    Runs n exepriments, where each experiment is training Q-learning agent on the same environment.
-    After one agent finished training, environment is reset to the initial state.
-    Parameters of the experiment:
-    :param n_episodes: number of episodes to perform in each experiment run
-    :param visualize: boolean flag if experiments should be rendered
-    :param n_experiments: number of experiments to perform.
-    :param log_level: level of logging that should be used by environment during experiments.
-    :param force: magnitude to be applied during experiment at each time step.
+                        mass_flow_nor=[0.75],
+                        weather_file='USA_CA_Riverside.Muni.AP.722869_TMY3.epw',
+                        npre_step=3,
+                        simulation_start_time=212*3600*24,
+                        time_step=15*60.,
+                        log_level=7):
 
-    Parameters of the cart pole environment:
-    :param m_cart: mass of cart.
-    :param m_pole: mass of a pole.
-    :param theta_0: angle of the pole. Is counted from the positive direction of X-axis. Specified in radians.
-    1/2*pi means pole standing straight on the cast.
-    :param theta_dot_0: angle speed of the poles mass center. I.e. how fast pole angle is changing.
-    :param time_step: time difference between simulation steps.
-    :param positive_reward: positive reward for RL agent.
-    :param negative_reward: negative reward for RL agent.
-    :return: trained Q-learning agent, array of actual episodes length
-    that were returned from cart_pole_train_qlearning()
-    """
-    config = {
-        'm_cart': m_cart,
-        'm_pole': m_pole,
-        'theta_0': theta_0,
-        'theta_dot_0': theta_dot_0,
-        'time_step': time_step,
-        'positive_reward': positive_reward,
-        'negative_reward': negative_reward,
-        'force': force,
-        'log_level': log_level
-    }
+#   from gym.envs.registration import register
+    import gym_singlezone_jmodelica
+    env_name = "JModelicaCSSingleZoneEnv-v0"
 
-#    from gym.envs.registration import register
-    import gym_cart_jmodelica
-    env_name = "JModelicaCSCartPoleEnv-v0"
-
-#    register(
-#        id=env_name,
-#        entry_point='examples:JModelicaCSCartPoleEnv',
-#        kwargs=config
-#    )
     trained_agent_s =[]
     episodes_length_s = []
     exec_time_s = []
     # env = gym.make(env, some_kwarg=your_vars)
     # need find a way to update the configuration at one time
     env = gym.make(env_name,
-        m_cart=m_cart,
-        m_pole=m_pole,
-        theta_0=theta_0,
-        theta_dot_0=theta_dot_0,
-        time_step=time_step,
-        positive_reward=positive_reward,
-        negative_reward=negative_reward,
-        force=force,
-        log_level=log_level)
+                mass_flow_nor=mass_flow_nor,
+                weather_file=weather_file,
+                npre_step=npre_step,
+                simulation_start_time=simulation_start_time,
+                time_step=time_step,
+                log_level=log_level)
         
     for i in range(n_experiments):
-        trained_agent, episodes_length, exec_time = cart_pole_train_qlearning(env,
-                                                                              n_episodes=n_episodes,
-                                                                              visualize=visualize)
+        trained_agent, episodes_length, exec_time = train_qlearning(env,
+                                                                    n_episodes=n_episodes,
+                                                                    visualize=visualize)
         trained_agent_s.append(trained_agent)
         episodes_length_s.append(episodes_length)
         exec_time_s.append(exec_time)
@@ -201,28 +188,16 @@ def run_ql_experiments(n_experiments=1,
     return trained_agent_s, episodes_length_s, exec_time_s
 
 
-#if __name__ == "__main__":
-#    _, episodes_lengths, exec_times = run_ql_experiments(visualize=True, log_level=logging.INFO)
-#    print("Experiment length {} s".format(exec_times[0]))
-#    print(u"Avg episode performance {} {} {}".format(episodes_lengths[0].mean(),
-#                                                     chr(177),  # plus minus sign
-#                                                     episodes_lengths[0].std()))
-#    print(u"Max episode performance {}".format(episodes_lengths[0].max()))
-#    print(u"All episodes performance {}".format(episodes_lengths))
-
 def run_experiment_with_result_files(folder,
-                                     n_experiments,
-                                     n_episodes,
-                                     visualize,
-                                     m_cart,
-                                     m_pole,
-                                     theta_0,
-                                     theta_dot_0,
-                                     time_step,
-                                     positive_reward,
-                                     negative_reward,
-                                     force,
-                                     log_level):
+                        n_experiments=1,
+                        n_episodes=10,
+                        visualize=False,
+                        mass_flow_nor=[0.75],
+                        weather_file='USA_CA_Riverside.Muni.AP.722869_TMY3.epw',
+                        npre_step=3,
+                        simulation_start_time=212*3600*24,
+                        time_step=15*60.,
+                        log_level=7):
     """
     Runs experiments with the given configuration and writes episodes length of all experiment as one file
     and execution times of experiments as another.
@@ -237,31 +212,20 @@ def run_experiment_with_result_files(folder,
     :param folder: folder for experiment result files
     :return: None
     """
-    experiment_file_name_prefix = "{}/experiment_{}_{}_{}_{}_{:.0f}_{}_{}_{}_{}_{}_".format(
+    experiment_file_name_prefix = "{}/experiment_{}_{}".format(
         folder,
         n_experiments,
-        n_episodes,
-        m_cart,
-        m_pole,
-        theta_0 * 180 / math.pi,
-        theta_dot_0,
-        time_step,
-        positive_reward,
-        negative_reward,
-        force
+        n_episodes
     )
     _, episodes_lengths, exec_times = run_ql_experiments(n_experiments=n_experiments,
                                                          n_episodes=n_episodes,
                                                          visualize=visualize,
-                                                         m_cart=m_cart,
-                                                         m_pole=m_pole,
-                                                         theta_0=theta_0,
-                                                         theta_dot_0=theta_dot_0,
-                                                         time_step=time_step,
-                                                         positive_reward=positive_reward,
-                                                         negative_reward=negative_reward,
-                                                         force=force,
-                                                         log_level=log_level)
+                                                        mass_flow_nor=mass_flow_nor,
+                                                        weather_file=weather_file,
+                                                        npre_step=npre_step,
+                                                        simulation_start_time=simulation_start_time,
+                                                        time_step=time_step,
+                                                        log_level=log_level)
     print(episodes_lengths)
     n =experiment_file_name_prefix + "episodes_lengths.csv"
     X = np.transpose(episodes_lengths)
@@ -276,30 +240,27 @@ def run_experiment_with_result_files(folder,
                delimiter=",",
                fmt='%10.4f')
 
-
-
-def reward_ratio_experiment(rws):
-    for pos_rew, neg_rew in rws:
-        run_experiment_with_result_files(folder,
-                                         n_experiments=1,
-                                         n_episodes=200,
-                                         visualize=False,
-                                         m_cart=10,
-                                         m_pole=1,
-                                         theta_0=85 / 180 * math.pi,
-                                         theta_dot_0=0,
-                                         time_step=0.05,
-                                         positive_reward=pos_rew,
-                                         negative_reward=neg_rew,
-                                         force=15,
-                                         log_level=logging.INFO)
-
-
 if __name__ == "__main__":
     import time
+    import os
+
     start = time.time()
     folder = "experiments_results"
+    
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
     # following experiments rake significant amount of time, so it is advised to run only one of them at once
-    reward_ratio_experiment([[1, -100], [1, -50], [1, -200],])
+    run_experiment_with_result_files(folder,
+                                        n_experiments=1,
+                                        n_episodes=1,
+                                        visualize=False,
+                                        mass_flow_nor=[0.75],
+                                        weather_file='USA_CA_Riverside.Muni.AP.722869_TMY3.epw',
+                                        npre_step=3,
+                                        simulation_start_time=212*3600*24.,
+                                        time_step=15*60.,
+                                        log_level=7)
+
     end = time.time()
     print("Total execution time {:.2f} seconds".format(end-start))
