3268a1d7882b3442f5782d4360bb370486247796
Yangyang Fu <yangyang.fu@tamu.edu>
2021-02-24 18:10:59 -0600

    Update mpc results using global optimziation method

---
+++ testcases/mpc/single-zone/mpc_final_run.bat
@@ -0,0 +1,11 @@
+docker run --name fmuc^
+      --user=root^
+	  --detach=false^
+	  -e DISPLAY=${DISPLAY}^
+	  -v /tmp/.X11-unix:/tmp/.X11-unix^
+	  --rm^
+	  -v %CD%:/mnt/shared^
+	  -i^
+      -t^
+	  mpcdrl_openopt /bin/bash -c "cd /mnt/shared && python /mnt/shared/mpc_final_run.py"
+
