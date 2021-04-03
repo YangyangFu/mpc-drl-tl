ebbd57b552fab0562438bcaac0f93925e04c6865
Yangyang Fu <yangyang.fu@tamu.edu>
2021-04-02 19:44:30 -0500

    Test reset fmu input methods

--- testcases/models/high_fidelity_models/single-zone/simulate_fmu.py
+++ testcases/models/high_fidelity_models/single-zone/simulate_fmu.py
@@ -10,20 +10,28 @@ import matplotlib.pyplot as plt
 # import fmu package
 from pyfmi import load_fmu
 from pymodelica import compile_fmu
+import numpy.random as random
 
+def uniform(a,b):
+    return (b-a)*random.random_sample()+a
 
 # simulate setup
-time_stop = 120. # 120s
+time_stop = 24*3600. # 120s
 startTime = 0
 endTime = startTime + time_stop
-dt = 60
+dt = 60*15.
 
 ## compile fmu - cs
 mopath = 'SingleZoneVAV.mo'
 modelpath = 'SingleZoneVAV.Airflow'
 fmu_name = "SingleZoneDamperControl"
-fmu = load_fmu(compile_fmu(modelpath,[mopath], target='cs',version='2.0',compile_to=fmu_name+'.fmu'))
+compiler_options = {"cs_rel_tol":1.0E-04}
+#fmu = load_fmu(compile_fmu(modelpath,[mopath], target='cs',version='2.0',compile_to=fmu_name+'.fmu', compiler_options=compiler_options))
+fmu = load_fmu(fmu_name+'.fmu')
 options = fmu.simulate_options()
+options['filter']=['uFan','TRoo','hvac.uFan']
+options['result_handling']="file" #"memory"
+
 options['ncp'] = 500.
 
 # initialize output
@@ -36,21 +44,28 @@ input_names = fmu.get_model_variables(causality = 2).keys()
 print input_names
 print('Inputs: {0}'.format(input_names))
 
-timeArray = np.arange(startTime,endTime,dt)
-uFan = 0.5*np.ones(timeArray.shape)
-u=np.transpose([timeArray.flatten(),uFan.flatten()])
-# input object as defined for jmdoelica fmu
-input_object = (input_names, u)
-
 # simulate fmu
 initialize = True
 
-for i in range(2):
-    ts = i*dt
-    options['initialize'] = initialize    
+ts = startTime
+while ts < endTime:
+    # settings
+    te = ts + dt
+    options['initialize'] = initialize  
 
-    res_step = fmu.simulate(input = input_object,start_time=ts, final_time=ts+dt, options=options)
+    # generate inputs
+    u = uniform(0.1,1)
+    #time = ts
+    #u_trajectory = np.vstack((time,u))
+    #input_object = (input_names,np.transpose(u_trajectory))
+    fmu.set(list(input_names),list([u]))
+    res_step = fmu.simulate(start_time=ts, final_time=te, options=options)
+
     initialize = False
+    ts = te
+
+    # get results
+    print res_step['hvac.uFan']
     print res_step['TRoo']-273.15
 
 print 'Finish simulation'
