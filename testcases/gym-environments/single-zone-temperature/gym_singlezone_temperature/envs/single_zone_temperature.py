d9e8cf402b709bf33de92f29e35c84cfabe9cf93
Yangyang Fu <yangyang.fu@tamu.edu>
2021-05-08 11:57:35 -0500

    correct temperature bounds for single zone temperature control environment

--- testcases/gym-environments/single-zone-temperature/gym_singlezone_temperature/envs/single_zone_temperature.py
+++ testcases/gym-environments/single-zone-temperature/gym_singlezone_temperature/envs/single_zone_temperature.py
@@ -115,7 +115,7 @@ class SingleZoneTemperatureEnv(object):
         T_upper = [30.0 for i in range(24)] # upper bound for unoccuppied: cooling
         T_lower = [12.0 for i in range(24)] # lower bound for unoccuppied: heating 
         T_upper[7:19] = [24.0]*12 # upper bound for occuppied: cooling 
-        T_lower[7:19] = [20.0]*12 # lower bound for occuppied: heating
+        T_lower[7:19] = [22.0]*12 # lower bound for occuppied: heating
         
         # control period:
         delCtrl = self.tau/3600.0 #may be better to set a variable in initial
