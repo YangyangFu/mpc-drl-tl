c2ae0569f951fceddfcc44bda6749355ff0a6d95
Yangyang Fu <yangyang.fu@colorado.edu>
2021-06-06 17:29:23 -0500

    update python3 supported docker

--- testcases/mpc/single-zone-fan-power/average-sample/test_mpc.bat
+++ testcases/mpc/single-zone-fan-power/average-sample/test_mpc.bat
@@ -7,4 +7,5 @@ docker run^
 	  -v %CD%:/mnt/shared^
 	  -i^
       -t^
-	  mpcdrl /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_singlezone.py"
+	  mpcdrl /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_mpc.py"
+
