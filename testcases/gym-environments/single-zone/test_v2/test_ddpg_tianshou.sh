d6c5b5e017133d5200c70fe0a508ef2a73e533a4
shichao2023 <shichaoxu2023@u.northwestern.edu>
2021-06-16 18:31:01 -0500

    update dqn, sac_discrete(bug), sac

---
+++ testcases/gym-environments/single-zone/test_v2/test_ddpg_tianshou.sh
@@ -0,0 +1,11 @@
+exec docker run --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=1\
+      --user=root \
+	  --detach=false \
+	  -e DISPLAY=${DISPLAY} \
+	  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	  --rm \
+	  -v `pwd`:/mnt/shared \
+	  -i \
+      -t \
+	  mpcdrl /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_ddpg_tianshou.py"  
+exit $
