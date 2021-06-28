f95a62a81af004f95a2780503706f85304c5a082
Yangyang Fu <yangyang.fu@tamu.edu>
2021-06-28 18:44:15 -0500

    update installation testing script

--- testcases/gym-environments/single-zone/test_v1/test_installation.py
+++ testcases/gym-environments/single-zone/test_v1/test_installation.py
@@ -5,31 +5,12 @@ import gym
 import gym_singlezone_jmodelica
 import tianshou as ts
 
-env = gym.make("JModelicaCSSingleZoneEnv-v0",
-                mass_flow_nor=0.75,
-                weather_file='USA_CA_Riverside.Muni.AP.722869_TMY3.epw',
-                npre_step=3,
-                simulation_start_time=3600*24,
-                time_step=15*60.,
-                log_level=7,
-                fmu_result_handling='memory',
-                fmu_result_ncp=100.,
-                filter_flag=True,
-                alpha=200,
-                nActions=101)
-states = env.reset()
-n_outputs = env.observation_space.shape[0]
-print(states)
-print(env.tau, env.simulation_start_time)
-print(env._get_action_space(),env.nActions)
-print(env.alpha)
-print(ts.__version__)
-
 env = gym.make("JModelicaCSSingleZoneEnv-v1",
                 mass_flow_nor=0.75,
                 weather_file='USA_CA_Riverside.Muni.AP.722869_TMY3.epw',
-                npre_step=3,
-                simulation_start_time=3600*24,
+                npre_step=5,
+                simulation_start_time=3600*24.,
+                simulation_end_time=3600*24*2.,
                 time_step=15*60.,
                 log_level=7,
                 fmu_result_handling='memory',
@@ -40,11 +21,12 @@ env = gym.make("JModelicaCSSingleZoneEnv-v1",
 states = env.reset()
 n_outputs = env.observation_space.shape[0]
 print(states)
-print(env.tau, env.simulation_start_time)
+print(n_outputs)
+print(env.tau, env.simulation_start_time, env.simulation_end_time)
 print(env._get_action_space(),env.nActions)
 print(env.alpha)
 print(ts.__version__)
-
+print("JModelicaCSSingleZoneEnv-v1 is successfully installed!!" )
 ## =========================================
 ##          Testing stablebaseline3 installation
 ##============================================
@@ -90,4 +72,4 @@ if __name__ == '__main__':
         action, _states = model.predict(obs)
         obs, rewards, dones, info = env.step(action)
         #env.render()
-    print("\n stablebaseline3 is successfully installed ! \n")
+print("\n stablebaseline3 is successfully installed ! \n")
