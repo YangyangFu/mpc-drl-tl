d2e98d88858346a37e8f4ecffe19526f43c5cd68
Yangyang Fu <yangyang.fu@tamu.edu>
2021-08-27 16:36:21 -0500

    Add ppo testing for single zone temperature env

---
+++ testcases/gym-environments/single-zone-temperature/test_v2/test_ppo_tianshou.bat
@@ -0,0 +1,11 @@
+docker run^
+      --user=root^
+	  --detach=false^
+	  -e DISPLAY=${DISPLAY}^
+	  -v /tmp/.X11-unix:/tmp/.X11-unix^
+	  --rm^
+	  -v %CD%:/mnt/shared^
+	  -i^
+      -t^
+	  mpcdrl /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_ppo_tianshou.py"
+
