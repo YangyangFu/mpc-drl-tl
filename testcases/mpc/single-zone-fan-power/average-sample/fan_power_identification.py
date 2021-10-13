fd6a8f3fe8addf4528ec2d4d14488da18840aa0a
Yangyang Fu <yangyang.fu@colorado.edu>
2021-10-13 18:20:55 -0500

    update power polynominal equations

--- testcases/mpc/single-zone-fan-power/average-sample/fan_power_identification.py
+++ testcases/mpc/single-zone-fan-power/average-sample/fan_power_identification.py
@@ -10,6 +10,7 @@ from scipy.optimize import curve_fit
 import json
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 def total_power(alpha, mz):
     """Predicte power at next step
@@ -38,49 +39,34 @@ def total_power(alpha, mz):
 
     alpha=np.array(alpha).reshape(-1)
     #beta=np.array(beta).reshape(-1)
-    P = alpha[0]+alpha[1]*mz+alpha[2]*mz**2 #+ beta[0]+ beta[1]*Toa+beta[2]*Toa**2
+    P = alpha[0]+alpha[1]*mz+alpha[2]*mz**2+alpha[3]*mz**3 #+ beta[0]+ beta[1]*Toa+beta[2]*Toa**2
 
-    return abs(P)
+    return P
 
 data = pd.read_csv('train_data.csv',index_col=[0])
+x = data['mass_flow'].values
+y = data['P_tot'].values
 
-# prepare data for fan power prediction
-"""l = 4
-P_his = pd.DataFrame(data['P_tot'])
-for i in range(l):
-    P_his['P_tot_'+str(i+1)] = data['P_tot'].values
-    shift = P_his['P_tot_'+str(i+1)].shift(periods=i+1)
-    P_his['P_tot_'+str(i+1)]=shift.values
-P_his=P_his.drop(columns=['P_tot'])
-data=pd.concat([data,P_his],axis=1)
-data.dropna(inplace=True)
-data.to_csv('prepared_data_power.csv')
-"""
+### prepare data for fitting
+# split data randomly
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
+# call curve fit
+# fit a power model
+def func_P(x,alpha1,alpha2,alpha3,alpha4):
 
-# split training and testing
-data_train = data.iloc[:int(20*24*4),:]
-data_test = data.iloc[20*24*4:-1,:]
-
-# fit a zone temperature model
-def func_P(x,alpha1,alpha2,alpha3):
-
-    alpha = np.array([alpha1,alpha2,alpha3])
+    alpha = np.array([alpha1,alpha2,alpha3,alpha4])
     y = total_power(alpha,x)
 
     return y
 
-x_train = data_train['mass_flow'].values
-y_train = data_train['P_tot'].values
-
-popt,pcov = curve_fit(func_P,x_train,y_train)
+popt,pcov = curve_fit(func_P,x_train,y_train, bounds=(0,np.inf))
 ypred_train = func_P(x_train,*popt)
 
 # test on testing data
-x_test = data_test['mass_flow'].values
-y_test = data_test['P_tot'].values
 ypred_test = func_P(x_test,*popt)
 
+### plot fitting results
 plt.figure()
 plt.subplot(311)
 plt.plot(y_train,'b-',lw=0.5,label='Target in Testing')
@@ -102,7 +88,20 @@ plt.legend()
 plt.savefig('Power.pdf')
 
 
-# export model parameter
+### check robustness
+x_fit = np.arange(0,0.75,0.01)
+print(x_fit)
+y_fit = [func_P(i,*popt) for i in x_fit]
+
+fig=plt.figure(figsize=[6,6])
+plt.scatter(x,y)
+plt.plot(x_fit,y_fit,'k-')
+plt.xlabel('mass flowrate [kg/s]')
+plt.ylabel('power [W]')
+plt.savefig('fan-power-fit.png')
+plt.savefig('fan-power-fit.pdf')
+
+### export model parameter
 popt_zone = {'alpha':list(popt)}
 
 with open('Power.json', 'w') as fp:
