178dc5a4f64f88856d8dcde97462d79c638a117b
Yangyang Fu <yangyang.fu@colorado.edu>
2021-10-26 23:36:27 -0500

    update python2 ann identificaiton pickle

--- testcases/mpc/single-zone-temperature/TZone_identification_ANN.sh
+++ testcases/mpc/single-zone-temperature/TZone_identification_ANN.sh
@@ -7,5 +7,5 @@
 	  -v `pwd`:/mnt/shared \
 	  -i \
       -t \
-	  mpcdrl /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/TZone_identification_ANN.py"
+	  yangyangfu/mpcdrl:cpu_py2  /bin/bash -c "cd /mnt/shared && python /mnt/shared/TZone_identification_ANN.py"
     exit $
