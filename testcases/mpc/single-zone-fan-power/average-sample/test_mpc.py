e44d3a104fa1135242d2ec475b9e2a4bbdf2e9fc
Yangyang Fu <yangyang.fu@tamu.edu>
2021-11-16 20:07:37 -0600

    revise casadi mpc formulation to output predictions

--- testcases/mpc/single-zone-fan-power/average-sample/test_mpc.py
+++ testcases/mpc/single-zone-fan-power/average-sample/test_mpc.py
@@ -8,7 +8,7 @@ import json
 # load testbed
 from pyfmi import load_fmu
 # import mpc
-from mpc import mpc_case
+from mpc_revise import mpc_case
 
 # get measurement
 def get_measurement(fmu_result,names):
