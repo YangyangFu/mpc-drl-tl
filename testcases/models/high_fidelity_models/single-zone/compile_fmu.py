971dc6db9f9cc4b0b6e740527c1e6553b100a3d1
Yangyang Fu <yangyang.fu@colorado.edu>
2022-02-20 16:42:19 -0700

    compile the FMU for future use

--- testcases/models/high_fidelity_models/single-zone/compile_fmu.py
+++ testcases/models/high_fidelity_models/single-zone/compile_fmu.py
@@ -12,8 +12,8 @@ from pymodelica import compile_fmu
 
 #### Compile Baseline Model
 ## ------------
-mopath = 'SingleZoneVAV.mo'
-modelpath = 'SingleZoneVAV.AirflowBaseline'
+mopath = 'SingleZoneFanCoilUnit.mo'
+modelpath = 'SingleZoneFanCoilUnit.TestCases.Baseline'
 # ------------
 
 # COMPILE FMU: set JVM maximum leap to 1G to avoid memory issues
@@ -21,13 +21,13 @@ modelpath = 'SingleZoneVAV.AirflowBaseline'
 # the defauted compilation target is model exchange, where no numerical integrator is integrated into the fmu. 
 # The equations in FMU is solved by numerical solvers in the importing tool.
 compiler_options = {"cs_rel_tol":1.0E-04}
-fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneDamperControlBaseline.fmu',compiler_options=compiler_options)
+fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneFCUBaseline.fmu',compiler_options=compiler_options)
 # -----------
 
 #### Compile AirFlow
 # ---------------------
-mopath = 'SingleZoneVAV.mo'
-modelpath = 'SingleZoneVAV.Airflow'
+mopath = 'SingleZoneFanCoilUnit.mo'
+modelpath = 'SingleZoneFanCoilUnit.TestCases.FanControl'
 # ------------
 
 # COMPILE FMU: set JVM maximum leap to 1G to avoid memory issues
@@ -35,7 +35,7 @@ modelpath = 'SingleZoneVAV.Airflow'
 # the defauted compilation target is model exchange, where no numerical integrator is integrated into the fmu. 
 # The equations in FMU is solved by numerical solvers in the importing tool.
 compiler_options = {"cs_rel_tol":1.0E-04}
-fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneDamperControl.fmu',compiler_options=compiler_options)
+fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneFCU.fmu',compiler_options=compiler_options)
 # -----------
 
 
