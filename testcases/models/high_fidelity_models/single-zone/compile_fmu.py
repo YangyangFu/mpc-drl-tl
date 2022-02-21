cfda4d950961932902ada3854b1bdf4611632327
Yangyang Fu <yangyang.fu@colorado.edu>
2022-02-20 17:46:26 -0700

    added fmu for jmodelica

--- testcases/models/high_fidelity_models/single-zone/compile_fmu.py
+++ testcases/models/high_fidelity_models/single-zone/compile_fmu.py
@@ -34,7 +34,6 @@ modelpath = 'SingleZoneFanCoilUnit.TestCases.FanControl'
 # -----------
 # the defauted compilation target is model exchange, where no numerical integrator is integrated into the fmu. 
 # The equations in FMU is solved by numerical solvers in the importing tool.
-compiler_options = {"cs_rel_tol":1.0E-04}
 fmupath = compile_fmu(modelpath,[mopath], jvm_args='-Xmx1g',target='cs',version='2.0',compile_to='SingleZoneFCU.fmu',compiler_options=compiler_options)
 # -----------
 
