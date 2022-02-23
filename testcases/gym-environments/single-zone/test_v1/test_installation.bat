37799b25ec0969b11b17347b1ac44243289e5dea
Yangyang Fu <yangyang.fu@tamu.edu>
2022-02-22 19:28:11 -0600

    remove not needed commands

--- testcases/gym-environments/single-zone/test_v1/test_installation.bat
+++ testcases/gym-environments/single-zone/test_v1/test_installation.bat
@@ -7,5 +7,5 @@ docker run^
 	  -v %CD%:/mnt/shared^
 	  -i^
       -t^
-	  mpcdrl /bin/bash -c "export TUNE_DISABLE_AUTO_CALLBACK_SYNCER=1 && source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_installation.py"
+	  mpcdrl_debug /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_installation.py"
 
