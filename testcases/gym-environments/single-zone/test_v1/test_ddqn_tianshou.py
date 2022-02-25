00d893b213e84c5043373bc1c84c80c4b89cf353
Yangyang Fu <yangyang.fu@tamu.edu>
2022-02-25 16:00:51 -0600

    use integer to avoid naming issues for postprocessing

--- testcases/gym-environments/single-zone/test_v1/test_ddqn_tianshou.py
+++ testcases/gym-environments/single-zone/test_v1/test_ddqn_tianshou.py
@@ -376,7 +376,7 @@ if __name__ == '__main__':
                 "stop": {"timesteps_total":args.step_per_epoch},
                 "config":{
                     "epoch": tune.grid_search([500]),
-                    "weight_energy": tune.grid_search([10, 100.]),
+                    "weight_energy": tune.grid_search([10, 100]),
                     "lr": tune.grid_search([3e-04]),
                     "batch_size": tune.grid_search([32, 64, 128]),
                     "n_hidden_layer": tune.grid_search([3]),
