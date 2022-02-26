a6cc0a6b4a5dce95bcdaa4924a9a85308ba2cb02
Yangyang Fu <yangyang.fu@colorado.edu>
2022-02-25 18:34:45 -0700

    udpate script for runing c51

--- testcases/gym-environments/single-zone/test_v1/test_c51_tianshou.sh
+++ testcases/gym-environments/single-zone/test_v1/test_c51_tianshou.sh
@@ -1,12 +1,12 @@
-exec docker run --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=3\
-      --user=root \
-	  --detach=false \
-	  -e DISPLAY=${DISPLAY} \
-	  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-	  --rm \
-	  -v `pwd`:/mnt/shared \
-	  -i \
-      -t \
-	  mpcdrl /bin/bash -c \
-	  "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_c51_tianshou.py"  
-exit $
+# add this to docker run for GPU: --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=3\
+docker run 
+	--user=root \
+	--shm-size=4.0gb \
+	--detach=false \
+	-e DISPLAY=${DISPLAY} \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	--rm \
+	-v `pwd`:/mnt/shared \
+	-i \
+	-t \
+	mpcdrl_debug /bin/bash -c "export TUNE_DISABLE_AUTO_CALLBACK_SYNCER=1 && source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_c51_tianshou.py"  
