2f4289a99b9cc4a211f636a020b5f5e38bb6069a
Yangyang Fu <fuyy2008@gmail.com>
2022-03-30 22:11:55 -0600

    added continuous version of action rewards

--- testcases/gym-environments/single-zone/gym_singlezone_jmodelica/__init__.py
+++ testcases/gym-environments/single-zone/gym_singlezone_jmodelica/__init__.py
@@ -38,3 +38,9 @@ register(
     entry_point='gym_singlezone_jmodelica.envs.single_zone_action_env_v1:JModelicaCSSingleZoneEnv',
     kwargs=config
 )
+
+register(
+    id='JModelicaCSSingleZoneEnv-action-v2',
+    entry_point='gym_singlezone_jmodelica.envs.single_zone_action_env_v2:JModelicaCSSingleZoneEnv',
+    kwargs=config
+)
--- testcases/gym-environments/single-zone/gym_singlezone_jmodelica/envs/__init__.py
+++ testcases/gym-environments/single-zone/gym_singlezone_jmodelica/envs/__init__.py
@@ -5,3 +5,4 @@ from gym_singlezone_jmodelica.envs import single_zone_action_env_v1
 # continuous action space
 from gym_singlezone_jmodelica.envs import single_zone_env_v2
 from gym_singlezone_jmodelica.envs import single_zone_price_env_v2
+from gym_singlezone_jmodelica.envs import single_zone_action_env_v2
