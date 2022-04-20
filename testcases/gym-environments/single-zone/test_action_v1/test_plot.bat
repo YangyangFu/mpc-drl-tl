f836138180d54834793ee4bf2adba19a93e686df
Fu <yangyang.fu@tamu.edu>
2022-04-20 14:04:33 -0500

    merger updates

--- testcases/gym-environments/single-zone/test_action_v1/test_plot.bat
+++ testcases/gym-environments/single-zone/test_action_v1/test_plot.bat
@@ -7,4 +7,4 @@ docker run^
 	  -v %CD%:/mnt/shared^
 	  -i^
       -t^
-	  mpcdrl_debug /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/tools.py --root-dir ./ddqn_tuning --algor ddqn --task JModelicaCSSingleZoneEnv-action-v1"
+	  mpcdrl_debug /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/tools.py --root-dir ./ddqn_tuning --algor ddqn --task JModelicaCSSingleZoneEnv-action-v1 --plot-test 1"
--- testcases/gym-environments/single-zone/test_action_v1/test_plot.sh
+++ testcases/gym-environments/single-zone/test_action_v1/test_plot.sh
@@ -8,5 +8,5 @@ exec docker run \
 	  -v `pwd`:/mnt/shared \
 	  -i \
       -t \
-	  mpcdrl_debug /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/tools.py --root-dir ./ddqn_tuning --algor ddqn --task JModelicaCSSingleZoneEnv-action-v1"  
+	  mpcdrl_debug /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/tools.py --root-dir ./ddqn_tuning --algor ddqn --task JModelicaCSSingleZoneEnv-action-v2 --plot-test 0"  
 exit $
