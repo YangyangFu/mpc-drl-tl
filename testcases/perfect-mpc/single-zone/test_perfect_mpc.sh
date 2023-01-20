522786843b0bcfcc63d69393faeb8f4618f014be
Yangyang Fu <fuyy2008@gmail.com>
2023-01-20 15:18:45 -0600

    clean folder and update documentation

--- testcases/perfect-mpc/single-zone/test_perfect_mpc.sh
+++ testcases/perfect-mpc/single-zone/test_perfect_mpc.sh
@@ -9,4 +9,4 @@ docker run \
 	-i \
 	-t \
 	yangyangfu/mpcdrl:cpu_py3 /bin/bash -c \
-	"export TUNE_DISABLE_AUTO_CALLBACK_SYNCER=1 && source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_pymoo.py"  
+	"export TUNE_DISABLE_AUTO_CALLBACK_SYNCER=1 && source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_perfect_mpc.py"  
