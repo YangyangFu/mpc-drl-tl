c15560f0120002b3403067e21a09600c3290d856
Mingyue Guo <moon_1735@163.com>
2023-05-30 15:50:32 -0500

    change ddqn log interval

--- testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
+++ testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
@@ -139,7 +139,7 @@ def test_dqn(args):
     log_path = os.path.join(args.logdir, args.task)
     writer = SummaryWriter(log_path)
     writer.add_text("args", str(args))
-    logger = TensorboardLogger(writer)
+    logger = TensorboardLogger(writer, train_interval =1)
     
     def save_fn(policy):
         torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
