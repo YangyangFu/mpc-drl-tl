84e8b1c9e76e80b18d71b69e7ab6395e36d27b15
Guo <mingyue.guo@tamu.edu>
2023-06-01 10:46:24 -0500

    revise reward

--- testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
+++ testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
@@ -345,43 +345,41 @@ if __name__ == '__main__':
     parser.add_argument('--weight-temp', type=float, default= 1.)   
     parser.add_argument('--weight-action', type=float, default=0.1)
     parser.add_argument('--lr', type=float, default=0.0001) #0.0003!!!!!!!!!!!!!!!!!!!!!
-    parser.add_argument('--epoch', type=int, default=50)
+    parser.add_argument('--epoch', type=int, default=200)
     parser.add_argument('--batch-size', type=int, default=256)
     parser.add_argument('--n-hidden-layers', type=int, default=3)
-    parser.add_argument('--buffer-size', type=int, default=50000) #12288
+    parser.add_argument('--buffer-size', type=int, default=12288) #12288
  
     args = parser.parse_args()
     #print(args)
     #Namespace(batch_size=128, buffer_size=50000, device='cpu', epoch=2, eps_test=0.005, eps_train=1.0, eps_train_final=0.05, frames_stack=1, gamma=0.99, logdir='log_ddqn', lr=0.0003, n_hidden_layers=3, n_step=1, resume_path=None, seed=0, step_per_collect=1, step_per_epoch=672, target_update_freq=96, task='SingleZoneEnv-ANN-v1', test_num=1, test_only=False, time_step=900.0, training_num=1, update_per_step=1, weight_action=0.1, weight_energy=100.0, weight_temp=1.0)
-    args.epoch = 2
-    print(args)
 
 
-    # # run one experiment
-    # test_dqn(args)
+    # run one experiment
+    test_dqn(args)
 
-    # Define Ray tuning experiments
-    tune.register_trainable("ddqn", trainable_function)
-    ray.init()
+    # # Define Ray tuning experiments
+    # tune.register_trainable("ddqn", trainable_function)
+    # ray.init()
 
-    # Run tuning
-    tune.run_experiments({
-        'ddqn_tuning': {
-            "run": "ddqn",
-            "stop": {"timesteps_total": args.step_per_epoch},
-            "config": {
-                "epoch": tune.grid_search([100]),
-                "weight_action": tune.grid_search([10]),
-                "lr": tune.grid_search([1e-6, 1e-4, 1e-3, 1e-2]),
-                "batch_size": tune.grid_search([256, 256*2]),
-                "n_hidden_layers": tune.grid_search([3]),
-                "buffer_size": tune.grid_search([4096*3]),
-                "seed":tune.grid_search([0])  # , 1, 2, 3, 4, 5
-                # "seed":tune.grid_search([0])
+    # # Run tuning
+    # tune.run_experiments({
+    #     'ddqn_tuning': {
+    #         "run": "ddqn",
+    #         "stop": {"timesteps_total": args.step_per_epoch},
+    #         "config": {
+    #             "epoch": tune.grid_search([100]),
+    #             "weight_action": tune.grid_search([10]),
+    #             "lr": tune.grid_search([1e-6, 1e-4, 1e-3, 1e-2]),
+    #             "batch_size": tune.grid_search([256, 256*2]),
+    #             "n_hidden_layers": tune.grid_search([3]),
+    #             "buffer_size": tune.grid_search([4096*3]),
+    #             "seed":tune.grid_search([0])  # , 1, 2, 3, 4, 5
+    #             # "seed":tune.grid_search([0])
 
-            },
-            "local_dir": "/mnt/shared",
-        }
-    })
+    #         },
+    #         "local_dir": "/mnt/shared",
+    #     }
+    # })
     toc = time.process_time()
     print ('======Finish in:' + str(toc-tic)+" second(s)==========")
