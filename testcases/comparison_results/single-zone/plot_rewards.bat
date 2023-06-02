e5a3c9955b0adab05426f145fcfe266904d0d575
Guo <mingyue.guo@tamu.edu>
2023-06-01 21:41:31 -0500

    tune max_eps_steps

---
+++ testcases/comparison_results/single-zone/plot_rewards.bat
@@ -0,0 +1,10 @@
+docker run^
+      --user=root^
+	  --detach=false^
+	  -e DISPLAY=${DISPLAY}^
+	  -v /tmp/.X11-unix:/tmp/.X11-unix^
+	  --rm^
+	  -v %CD%:/mnt/shared^
+	  -i^
+      -t^
+	  mpcdrl_debug:latest /bin/bash -c "source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/plot_rewards.py --root-dir ./test --root-dir ./DRL-R2"
--- testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
+++ testcases/gym-environments/single-zone-rom/test_ann/test_ddqn_tianshou.py
@@ -202,7 +202,7 @@ def test_dqn(args):
     def train_fn(epoch, env_step):
         # nature DQN setting, linear decay in the first 1M steps
         #max_eps_steps = int(args.epoch * args.step_per_epoch * 0.9) # this will not help speedup learning with large epoch
-        max_eps_steps =200*96*7 # linear decay in the first about 100000 steps. 
+        max_eps_steps =args.max_eps_steps # linear decay in the first about 100000 steps. 
         #print("observe eps:  max_eps_steps, total_epoch_pass ", max_eps_steps, total_epoch_pass)
         if env_step <= max_eps_steps:
             eps = args.eps_train - env_step * (args.eps_train - args.eps_train_final) / max_eps_steps
@@ -247,7 +247,7 @@ def test_dqn(args):
         pprint.pprint(result)
         import pandas as pd
         rew_df = pd.DataFrame(rew_rec, columns = ['Reward'])
-        averages = rew_df.groupby(rew_df.index // 96)["Reward"].mean()
+        averages = rew_df.groupby(rew_df.index // args.step_per_epoch)["Reward"].mean()
         tra_aver_rew = pd.DataFrame(averages, columns = ['Reward'])
         tra_aver_rew.to_csv(os.path.join(args.logdir, args.task, 'tra_aver_rew.csv'))
 
@@ -287,6 +287,40 @@ def test_dqn(args):
         print(f'Mean reward (over {result["n/ep"]} episodes): {rew}')
     watch()
 
+    # def train_watch():
+    #     print("Setup test envs ...")
+    #     policy.eval()
+    #     policy.set_eps(args.eps_test)
+        
+    #     buffer = VectorReplayBuffer(
+    #         args.step_per_epoch,
+    #         buffer_num=len(test_envs),
+    #         ignore_obs_next=True,
+    #         save_only_last_obs=False,
+    #         stack_num=args.frames_stack)
+    #     collector = Collector(policy, test_envs, buffer)
+    #     result = collector.collect(n_step=args.step_per_epoch)
+
+    #     # get obs mean and var
+    #     obs_mean = test_envs.obs_rms.mean
+    #     obs_var = test_envs.obs_rms.var
+    #     print(obs_mean)
+    #     print(obs_var)
+    #     # the observations and action may be normalized depending on training setting
+    #     np.save(os.path.join(args.logdir, args.task, 'his_act.npy'),
+    #             buffer._meta.__dict__['act'])
+    #     np.save(os.path.join(args.logdir, args.task, 'his_obs.npy'),
+    #             buffer._meta.__dict__['obs'])
+    #     np.save(os.path.join(args.logdir, args.task, 'his_rew.npy'),
+    #             buffer._meta.__dict__['rew'])
+    #     np.save(os.path.join(args.logdir, args.task, 'obs_mean.npy'),
+    #             obs_mean)
+    #     np.save(os.path.join(args.logdir, args.task, 'obs_var.npy'),
+    #             obs_var)
+    #     rew = result["rews"].mean()
+    #     print(f'Mean reward (over {result["n/ep"]} episodes): {rew}')
+
+
 # added hyperparameter tuning scripting using Ray.tune
 def trainable_function(config, reporter):
     while True:
@@ -321,6 +355,8 @@ if __name__ == '__main__':
     parser.add_argument('--eps-test', type=float, default=0.005)
     parser.add_argument('--eps-train', type=float, default=1.)
     parser.add_argument('--eps-train-final', type=float, default=0.05)
+    parser.add_argument('--max-eps-steps', type=float, default=0.5*max_number_of_steps*50)
+    
 
     parser.add_argument('--gamma', type=float, default=0.99)
 
@@ -343,43 +379,43 @@ if __name__ == '__main__':
     # tunable parameters
     parser.add_argument('--weight-energy', type=float, default= 100.)   
     parser.add_argument('--weight-temp', type=float, default= 1.)   
-    parser.add_argument('--weight-action', type=float, default=0.1)
+    parser.add_argument('--weight-action', type=float, default=1.) #0.1
     parser.add_argument('--lr', type=float, default=0.0001) #0.0003!!!!!!!!!!!!!!!!!!!!!
-    parser.add_argument('--epoch', type=int, default=200)
-    parser.add_argument('--batch-size', type=int, default=256)
-    parser.add_argument('--n-hidden-layers', type=int, default=3)
-    parser.add_argument('--buffer-size', type=int, default=12288) #12288
+    parser.add_argument('--epoch', type=int, default=50)
+    parser.add_argument('--batch-size', type=int, default=96*2) #256
+    parser.add_argument('--n-hidden-layers', type=int, default=4) #3
+    parser.add_argument('--buffer-size', type=int, default=96*15) #12288
  
     args = parser.parse_args()
     #print(args)
     #Namespace(batch_size=128, buffer_size=50000, device='cpu', epoch=2, eps_test=0.005, eps_train=1.0, eps_train_final=0.05, frames_stack=1, gamma=0.99, logdir='log_ddqn', lr=0.0003, n_hidden_layers=3, n_step=1, resume_path=None, seed=0, step_per_collect=1, step_per_epoch=672, target_update_freq=96, task='SingleZoneEnv-ANN-v1', test_num=1, test_only=False, time_step=900.0, training_num=1, update_per_step=1, weight_action=0.1, weight_energy=100.0, weight_temp=1.0)
 
 
-    # run one experiment
-    test_dqn(args)
+    # # run one experiment
+    # test_dqn(args)
 
-    # # Define Ray tuning experiments
-    # tune.register_trainable("ddqn", trainable_function)
-    # ray.init()
+    # Define Ray tuning experiments
+    tune.register_trainable("ddqn", trainable_function)
+    ray.init()
 
-    # # Run tuning
-    # tune.run_experiments({
-    #     'ddqn_tuning': {
-    #         "run": "ddqn",
-    #         "stop": {"timesteps_total": args.step_per_epoch},
-    #         "config": {
-    #             "epoch": tune.grid_search([100]),
-    #             "weight_action": tune.grid_search([10]),
-    #             "lr": tune.grid_search([1e-6, 1e-4, 1e-3, 1e-2]),
-    #             "batch_size": tune.grid_search([256, 256*2]),
-    #             "n_hidden_layers": tune.grid_search([3]),
-    #             "buffer_size": tune.grid_search([4096*3]),
-    #             "seed":tune.grid_search([0])  # , 1, 2, 3, 4, 5
-    #             # "seed":tune.grid_search([0])
-
-    #         },
-    #         "local_dir": "/mnt/shared",
-    #     }
-    # })
+    # Run tuning
+    tune.run_experiments({
+        'ddqn_tuning': {
+            "run": "ddqn",
+            "stop": {"timesteps_total": args.step_per_epoch},
+            "config": {
+                "epoch": tune.grid_search([50]),
+                "weight_action": tune.grid_search([10]),
+                "lr": tune.grid_search([1e-4]),
+                # "lr": tune.uniform(0.5 * 1e-4, 0.5 *1e-3),
+                "batch_size": tune.grid_search([96*2]), # 256
+                "n_hidden_layers": tune.grid_search([4]),
+                "buffer_size": tune.grid_search([96*15]),  #4096*3
+                "seed":tune.grid_search([0]),  # , 1, 2, 3, 4, 5
+                "max_eps_steps":tune.grid_search([0.2*50*96, 0.4*50*96, 0.6*50*96])
+            },
+            "local_dir": "/mnt/shared",
+        }
+    })
     toc = time.process_time()
     print ('======Finish in:' + str(toc-tic)+" second(s)==========")
