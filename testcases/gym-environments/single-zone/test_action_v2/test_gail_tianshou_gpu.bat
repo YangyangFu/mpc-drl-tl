13623b8d983d079ef34ba7ecd487bf585051c5f5
Mingzhe Liu <46808578+mingzhe37@users.noreply.github.com>
2023-12-18 21:33:39 -0600

    added gpu bat

---
+++ testcases/gym-environments/single-zone/test_action_v2/test_gail_tianshou_gpu.bat
@@ -0,0 +1,12 @@
+docker run^
+      --user=root^
+	  --gpus all^
+	  --shm-size=6.03gb^
+	  --detach=false^
+	  -e DISPLAY=${DISPLAY}^
+	  -v /tmp/.X11-unix:/tmp/.X11-unix:rw^
+	  --rm^
+	  -v %CD%:/mnt/shared^
+	  -i^
+      -t^
+	  mingzhe37/mpc-drl-gpu:v0 /bin/bash -c "export TUNE_DISABLE_AUTO_CALLBACK_SYNCER=1 && source activate base && export PYTHONPATH=$PYFMI_PY3_CONDA_PATH:$PYTHONPATH && cd /mnt/shared && python /mnt/shared/test_gail_tianshou_gpu.py"  
